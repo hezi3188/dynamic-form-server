04cb29ebb3d44c0323200c6fbec2dd9df3a47a05
Yehezkel Ben Atar <hezi3188@gmail.com>
2025-03-02 21:45:40 +0200

    add host

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app: Application = express();
 
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: ['http://localhost:5173', 'https://dynamic-form-server.onrender.com'],
     })
 );
 
